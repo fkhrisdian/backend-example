3095e3e011148519e2a7aaaf8736da8919815912
fkhrisdian@gmail.com <Johnterry@26>
2020-06-29 17:13:06 +0700

    get audit trail now sort by descending created date

--- src/main/java/com/kaspro/bank/persistance/repository/TrailAuditRepository.java
+++ src/main/java/com/kaspro/bank/persistance/repository/TrailAuditRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface TrailAuditRepository extends BaseRepository<TrailAudit>{
-    @Query(value="SELECT * FROM kasprobank.AUDIT_TRAIL where owner_id=?1",
+    @Query(value="SELECT * FROM kasprobank.AUDIT_TRAIL where owner_id=?1 order by  kasprobank.AUDIT_TRAIL.DATE_CREATED desc",
             nativeQuery = true)
     List<TrailAudit> findByOwnerID(String id);
 }
